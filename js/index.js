8312bc68acbb8337ca69cf5e259b6b54f785419f
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2025-01-13 15:30:45 -0500

    add starting js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 
+// Element Definitions
+var usernameInput = document.getElementById("username-inp")
+var passwordInput = document.getElementById("password-inp")
+
 // Buttons
 var loginButtonEl = document.getElementById("login-btn")
 
 // Click Behaviors
 loginButtonEl.onclick = function () {
-    
-    window.location.href = "TicketList.html";
+    init_login()
+    // window.location.href = "TicketList.html";
 };
+
+function init_login() {
+    const username = usernameInput.value
+    const password = passwordInput.value
+    console.log(username)
+    console.log(password)
+    
+}
