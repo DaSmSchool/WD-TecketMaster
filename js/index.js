afede9fb413bec37a88db9549028e8877960d5c4
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2025-01-13 15:20:19 -0500

    add button ids

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 
 // Buttons
-var loginButtonEl = document.getElementById("myButton")
+var loginButtonEl = document.getElementById("login-btn")
+
+// Click Behaviors
 loginButtonEl.onclick = function () {
+    
     window.location.href = "TicketList.html";
 };
