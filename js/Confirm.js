e3a6794a123f1c43cc527ff18f8ec760403d309a
DaSmSchool <145377174+DaSmSchool@users.noreply.github.com>
2025-01-17 15:28:03 -0500

    add html progress

---
+++ js/Confirm.js
@@ -0,0 +1,7 @@
+// Buttons
+var homeButtonEl = document.getElementById("return-home-btn")
+
+// Click Behaviors
+homeButtonEl.onclick = function () {
+    window.location.href = "TicketList.html";
+};
